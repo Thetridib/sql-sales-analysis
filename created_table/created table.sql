b1b424ae60306ba815e3e133a4094db749ae96ae
Tridib Sarkar <107795563+Thetridib@users.noreply.github.com>
2025-11-28 20:15:26 +0530

    Add files via upload

---
+++ created_table/created table.sql
@@ -0,0 +1,25 @@
+--created the tables
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100),
+    city VARCHAR(50)
+);
+
+CREATE TABLE products (
+    product_id INT PRIMARY KEY,
+    product_name VARCHAR(100),
+    category VARCHAR(50),
+    price NUMERIC(10,2)
+);
+
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT REFERENCES customers(customer_id),
+    product_id INT REFERENCES products(product_id),
+    order_date DATE,
+    quantity INT
+);
+SELECT * FROM customers;
+SELECT * FROM products;
+SELECT * FROM orders;
